e6de8d92507501c8a4e2438b1b625e1b0771dd78
Ashley Lawrence <ash.l.96@outlook.com>
2024-11-21 16:35:25 +0000

    Set up POST request handler

--- about/views.py
+++ about/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 
 
 def about_me(request):
     """
-    Renders the About page.
+    Renders the About page and handles the collaboration request form.
     """
     about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method == 'POST':
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaboration = collaborate_form.save(commit=False)
+            collaboration.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Your collaboration request has been submitted! I will endeavor to respond within 2 working days.'
+            )
+    else:
+        collaborate_form = CollaborateForm()
+
+
     return render(
-        request, 'about/about.html', {'about': about_me},
+        request, 'about/about.html', {
+            'about': about,
+            'collaborate_form': collaborate_form,
+        },
     )
